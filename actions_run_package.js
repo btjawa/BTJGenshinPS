cf0cc6fc0fc060b9e0f212c420f653fec9ee99ac
btjawa <2407356083@qq.com>
2023-09-14 11:06:38 +0800

    Update actions_run_package.js

--- actions_run_package.js
+++ actions_run_package.js
@@ -5,19 +5,16 @@ const path = require('path');
 const { config } = require('process');
 const version = "1.0.0";
 
-const localesDir = path.join(__dirname, `BTJGenshinPS-${version}-win32-ia32-alpha`, 'locales');
-let error, stderr, stdout;
-
 async function package() {
   try {
     ({error, stderr, stdout} = await exec(`node_modules\\.bin\\electron-packager . BTJGenshinPS --platform=win32 --overwrite --icon=./dist/favicon.ico --arch=ia32 --asar --ignore=GateServer --ignore=data`));
     error ? config.error(error) : console.log(`${stdout}\n${stderr}`)
 
-    const files = await fs.readdir(localesDir);
+    const files = await fs.readdir(`BTJGenshinPS-win32-ia32`);
     const filesToKeep = ['en-US.pak', 'zh-CN.pak', 'zh-TW.pak'];
     for (const file of files) {
       if (!filesToKeep.includes(file)) {
-        await fs.unlink(path.join(localesDir, file));
+        await fs.unlink(path.join(`BTJGenshinPS-win32-ia32`, file));
       }
     }
   } catch (error) {
